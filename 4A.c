e41bc75aae335315d79028cf4c84934e0a77f078
Mumetu Mahadin Jehad <mumetumahadinjehad@gmail.com>
2025-04-12 15:15:43 +0600

    Add files via upload

---
+++ 4A.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+       if(num%2!=0)
+      { printf("NO\n");}
+       else 
+       {
+          if(num==2)
+          printf("NO\n");
+          else 
+          printf("YES\n");
+       }
+    return 0;
+}
